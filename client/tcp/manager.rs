9d0ceb5c5c246a75fd057aebbe15b3c071cda852
Noah Caldwell <noah_caldwell2@baylor.edu>
2025-04-18 11:05:58 -0500

    added tcp directory to client due to unresolved import

---
+++ client/tcp/manager.rs
@@ -0,0 +1,17 @@
+// src/tcp/manager.rs
+pub struct Manager {
+    // fields here would go here
+}
+
+impl Manager {
+    pub fn new() -> Self {
+        Manager {
+            // Initialize stuff
+        }
+    }
+
+    pub async fn forward_packet(/* params */) -> Result<(), Box<dyn std::error::Error>> {
+        //  logic
+        Ok(())
+    }
+}
---
+++ client/tcp/mod.rs
@@ -0,0 +1,3 @@
+// src/tcp/mod.rs
+
+pub mod manager;

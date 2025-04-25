205edceaecf8e220ee82f363c3582fba0019b2ec
Renzenar <118459019+Renzenar@users.noreply.github.com>
2025-04-25 14:44:04 -0500

    Added ServerResponse object

--- shared/src/client_response.rs
+++ shared/src/client_response.rs
@@ -7,6 +7,14 @@ pub struct ClientRequest {
 }
 
 #[derive(Debug, Serialize, Deserialize)]
+pub struct ServerResponse {
+    pub jwt: Option<String>,
+    pub success: bool,
+    pub message: Option<String>,
+    pub data: serde_json::Value,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
 #[serde(tag = "type", content = "data")]
 pub enum Command {
     Login {

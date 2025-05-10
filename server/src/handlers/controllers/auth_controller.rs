d929f68fa360cf4d40560e3f05fe02a317254af6
Noah Caldwell <noah_caldwell2@baylor.edu>
2025-05-10 12:57:56 -0500

    fixing merge conflucts

--- server/src/handlers/controllers/auth_controller.rs
+++ server/src/handlers/controllers/auth_controller.rs
@@ -3,6 +3,7 @@ use sea_orm::DatabaseConnection;
 use crate::handlers::services::auth_service;
 use crate::utils::errors::server_error::ServerError;
 use shared::models::auth_models::AuthResponseModel;
+use shared::models::server_models::ServerResponseModel;
 
 pub async fn register(
     username: String,
@@ -20,4 +21,12 @@ pub async fn login(
 ) -> Result<AuthResponseModel, ServerError> {
     // Call login service
     auth_service::login(username, password, db).await
+}
+
+pub async fn update_password(
+    jwt: String,
+    new_password: String,
+    db: Arc<DatabaseConnection>
+) -> Result<ServerResponseModel, ServerError> {
+    auth_service::update_password(jwt, new_password, db).await
 }
